0997ef86999ce673f64964b3f478531884e793d0
Nep-Timeline <nep_timeline@outlook.com>
2023-11-08 13:09:36 +0800

    Update Nekogram.java

--- app/src/main/java/nep/timeline/re_telegram/obfuscate/resolves/Nekogram.java
+++ app/src/main/java/nep/timeline/re_telegram/obfuscate/resolves/Nekogram.java
@@ -13,19 +13,19 @@ public class Nekogram {
     private static final List<MethodInfo> methodList = new ArrayList<>();
 
     static {
-        classList.add(new ClassInfo("org.telegram.messenger.ApplicationLoader", "Q8"));
-        classList.add(new ClassInfo("org.telegram.messenger.NotificationsController", "EI0"));
-        classList.add(new ClassInfo("org.telegram.messenger.NotificationCenter", "cH0"));
-        classList.add(new ClassInfo("org.telegram.messenger.MessagesController", "dC0"));
-        classList.add(new ClassInfo("org.telegram.messenger.MessagesStorage", "jD0"));
-        classList.add(new ClassInfo("org.telegram.messenger.MessageObject", "lz0"));
-        classList.add(new ClassInfo("org.telegram.messenger.UserConfig", "zA1"));
+        classList.add(new ClassInfo("org.telegram.messenger.ApplicationLoader", "R8"));
+        classList.add(new ClassInfo("org.telegram.messenger.NotificationsController", "GI0"));
+        classList.add(new ClassInfo("org.telegram.messenger.NotificationCenter", "eH0"));
+        classList.add(new ClassInfo("org.telegram.messenger.MessagesController", "gC0"));
+        classList.add(new ClassInfo("org.telegram.messenger.MessagesStorage", "lD0"));
+        classList.add(new ClassInfo("org.telegram.messenger.MessageObject", "oz0"));
+        classList.add(new ClassInfo("org.telegram.messenger.UserConfig", "BA1"));
         classList.add(new ClassInfo("org.telegram.ui.Cells.ChatMessageCell", "org.telegram.ui.Cells.r"));
-        classList.add(new ClassInfo("org.telegram.ui.ActionBar.Theme", "bt1"));
+        classList.add(new ClassInfo("org.telegram.ui.ActionBar.Theme", "dt1"));
         classList.add(new ClassInfo("org.telegram.ui.ChatActivity", "org.telegram.ui.m3"));
         classList.add(new ClassInfo("org.telegram.ui.DialogsActivity", "org.telegram.ui.n4"));
         classList.add(new ClassInfo("org.telegram.ui.FiltersSetupActivity$TouchHelperCallback", "org.telegram.ui.s1"));
-        classList.add(new ClassInfo("org.telegram.messenger.AndroidUtilities", "x7"));
+        classList.add(new ClassInfo("org.telegram.messenger.AndroidUtilities", "y7"));
 
         fieldList.add(new FieldInfo("MessageObject", "messageOwner", "j"));
         fieldList.add(new FieldInfo("UserConfig", "selectedAccount", "G0"));
@@ -42,14 +42,14 @@ public class Nekogram {
         methodList.add(new MethodInfo("MessagesController", "markDialogMessageAsDeleted", "V1"));
         methodList.add(new MethodInfo("MessagesController", "deleteMessages", "R"));
         methodList.add(new MethodInfo("MessagesController", "getInstance", "N0"));
-        methodList.add(new MethodInfo("ChatMessageCell", "measureTime", "Z4"));
+        methodList.add(new MethodInfo("ChatMessageCell", "measureTime", "d5"));
         methodList.add(new MethodInfo("UserConfig", "getInstance", "g"));
         methodList.add(new MethodInfo("UserConfig", "isPremium", "o"));
         methodList.add(new MethodInfo("NotificationsController", "removeNotificationsForDialog", "I"));
         methodList.add(new MethodInfo("NotificationsController", "removeDeletedMessagesFromNotifications", "H"));
         //methodList.add(new MethodInfo("NotificationsController", "loadRoundAvatar", "w"));
-        methodList.add(new MethodInfo("ChatActivity", "addSponsoredMessages", "pg"));
-        methodList.add(new MethodInfo("ChatActivity", "hasSelectedNoforwardsMessage", "mi"));
+        methodList.add(new MethodInfo("ChatActivity", "addSponsoredMessages", "rg"));
+        methodList.add(new MethodInfo("ChatActivity", "hasSelectedNoforwardsMessage", "oi"));
         methodList.add(new MethodInfo("DialogsActivity", "onDefaultTabMoved", "B0"));
         methodList.add(new MethodInfo("FiltersSetupActivity$TouchHelperCallback", "resetDefaultPosition", "o"));
         methodList.add(new MethodInfo("AndroidUtilities", "getTypeface", "N0"));
