865338a5faa2598e45a8a24a7b380a4357cc6f0a
Henrik <15855905+high3eam@users.noreply.github.com>
2025-02-13 20:24:32 +0100

    Update SpeedBoostHook.java

--- app/src/main/java/nep/timeline/re_telegram/features/SpeedBoostHook.java
+++ app/src/main/java/nep/timeline/re_telegram/features/SpeedBoostHook.java
@@ -26,28 +26,6 @@ public class SpeedBoostHook {
                     XposedHelpers.setObjectField(param.thisObject, "maxDownloadRequests", maxDownloadRequests);
                     XposedHelpers.setObjectField(param.thisObject, "maxDownloadRequestsBig", maxDownloadRequestsBig);
                     XposedHelpers.setObjectField(param.thisObject, "maxCdnParts", maxCdnParts);
-
-                    try {
-                        var fileSize = XposedHelpers.getLongField(param.thisObject, "totalBytesCount");
-                        if (fileSize > 5 * 1024 * 1024 && System.currentTimeMillis() - speedUpShown > 1000 * 60 * 2) {
-                            speedUpShown = System.currentTimeMillis();
-                            var title = "Speed Boost Activated";
-                            var subtitle = "Extreme Speed Mode Enabled";
-                            try {
-                                var notificationCenterClass = XposedHelpers.findClass("org.telegram.messenger.NotificationCenter", classLoader);
-                                var globalInstance = XposedHelpers.callStaticMethod(notificationCenterClass, "getGlobalInstance");
-                                new Handler(Looper.getMainLooper()).post(() -> XposedHelpers.callMethod(
-                                        globalInstance,
-                                        "postNotificationName",
-                                        XposedHelpers.getStaticIntField(notificationCenterClass, "showBulletin"),
-                                        new Object[]{4, title, subtitle}));
-                            } catch (Throwable t) {
-                                XposedBridge.log(t);
-                            }
-                        }
-                    } catch (Throwable t) {
-                        XposedBridge.log(t);
-                    }
                 }
             });
         } catch (Throwable t) {
