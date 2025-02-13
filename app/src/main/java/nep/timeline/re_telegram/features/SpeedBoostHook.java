6f947bdc9f9e1eb22e2a089bcc7961437e5552cd
Henrik <15855905+high3eam@users.noreply.github.com>
2025-02-13 20:31:08 +0100

    Update SpeedBoostHook.java

--- app/src/main/java/nep/timeline/re_telegram/features/SpeedBoostHook.java
+++ app/src/main/java/nep/timeline/re_telegram/features/SpeedBoostHook.java
@@ -1,17 +1,12 @@
 package nep.timeline.re_telegram.features;
-
 import android.os.Handler;
 import android.os.Looper;
-
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
-import nep.timeline.re_telegram.Utils;
 
 public class SpeedBoostHook {
     private final static long DEFAULT_MAX_FILE_SIZE = 1024L * 1024L * 2000L;
-    private static long speedUpShown = 0;
-
     public static void init(ClassLoader classLoader) {
         try {
             XposedHelpers.findAndHookMethod("org.telegram.messenger.FileLoadOperation", classLoader, "updateParams", new XC_MethodHook() {
@@ -20,7 +15,6 @@ public class SpeedBoostHook {
                     int downloadChunkSizeBig = 1024 * 1024;
                     int maxDownloadRequests = 12;
                     int maxDownloadRequestsBig = 12;
-
                     var maxCdnParts = (int) (DEFAULT_MAX_FILE_SIZE / downloadChunkSizeBig);
                     XposedHelpers.setIntField(param.thisObject, "downloadChunkSizeBig", downloadChunkSizeBig);
                     XposedHelpers.setObjectField(param.thisObject, "maxDownloadRequests", maxDownloadRequests);
